c7002505026ac716d8aac39fa543d62f6789f844
Chanapai Chuadchum <kornbot359@hotmail.com>
2022-02-13 17:57:11 +0700

    Update requestmouserapi.py

--- requestmouserapi.py
+++ requestmouserapi.py
@@ -5,7 +5,7 @@ import requests
 from requests.structures import CaseInsensitiveDict
 import subprocess
 curl = 'curl -X POST "https://api.mouser.com/api/v1/search/partnumber?apiKey=cf621336-fea9-4734-81dd-a8442ed3e05c" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"SearchByPartRequest\": { \"mouserPartNumber\": \"LM324\", \"partSearchOptions\": \"1\" }}"'
-curl1 = 'curl -X POST "https://api.mouser.com/api/v1/search/keyword?" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"SearchByKeywordRequest\": { \"keyword\": \"DC motor driver IC\", \"records\": 0, \"startingRecord\": 0, \"searchOptions\": \"1\", \"searchWithYourSignUpLanguage\": \"English\" }}"'
+curl1 = 'curl -X POST "https://api.mouser.com/api/v1/search/keyword?apiKey=cf621336-fea9-4734-81dd-a8442ed3e05c" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"SearchByKeywordRequest\": { \"keyword\": \"DC motor driver IC\", \"records\": 0, \"startingRecord\": 0, \"searchOptions\": \"1\", \"searchWithYourSignUpLanguage\": \"English\" }}"'
 headers = CaseInsensitiveDict()
 headers["Content-Type"] = "application/json"
 
