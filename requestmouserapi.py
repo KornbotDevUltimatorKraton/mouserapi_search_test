0cd0be477d80a716d8d3674037a03c6150c2bb63
Chanapai Chuadchum <kornbot359@hotmail.com>
2022-02-13 19:28:24 +0700

    Update requestmouserapi.py

--- requestmouserapi.py
+++ requestmouserapi.py
@@ -4,13 +4,13 @@ from urllib import request
 import requests
 from requests.structures import CaseInsensitiveDict
 import subprocess
+#Curl link for testing the api post request
 curl = 'curl -X POST "https://api.mouser.com/api/v1/search/partnumber?apiKey=cf621336-fea9-4734-81dd-a8442ed3e05c" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"SearchByPartRequest\": { \"mouserPartNumber\": \"LM324\", \"partSearchOptions\": \"1\" }}"'
 curl1 = 'curl -X POST "https://api.mouser.com/api/v1/search/keyword?apiKey=cf621336-fea9-4734-81dd-a8442ed3e05c" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"SearchByKeywordRequest\": { \"keyword\": \"DC motor driver IC\", \"records\": 0, \"startingRecord\": 0, \"searchOptions\": \"1\", \"searchWithYourSignUpLanguage\": \"English\" }}"'
 headers = CaseInsensitiveDict()
 headers["Content-Type"] = "application/json"
 
 data = { 
-  'apiKey':'cf621336-fea9-4734-81dd-a8442ed3e05c',
   'SearchByKeywordRequest': {
     'keyword': 'DC motor driver IC',
     'records': 0,
